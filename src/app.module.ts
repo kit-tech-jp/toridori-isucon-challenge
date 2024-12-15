446c90697bdaabaca8f75ee157dcdbd5824517d0
Ikeda Naohito <ikeda@IkedaMacBook-Pro.local>
2024-12-15 12:16:24 +0900

    fix: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { LoggerMiddleware } from "./access-log";
 import { PrismaModule } from "./db/prisma.module";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
   imports: [PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
